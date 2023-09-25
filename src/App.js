2e2a9e379248fc5f3e71aeac9cd15934de999eca
Dmytro Kurmaiev <d.kurmaiev@gmail.com>
2023-09-25 14:41:31 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
       
       <div className="App">
         <Header />
+        
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<AboutUs />} />
